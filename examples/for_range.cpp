139299f53cb9f0f3b6f801102184475b3530e3fb
Pedro Larroy <pedro.larroy@gmail.com>
2011-11-09 00:13:33 +0100

    for_range

---
+++ examples/for_range.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    vector<int> vi = {1,2,3,4};
+    for (auto i: vi)
+        cout << i << endl;
+}
