7a1d6f39df0ba30d8aa54b596919441b82b13491
Pedro Larroy <pedro.larroy@gmail.com>
2011-11-09 00:14:49 +0100

    use static initializers

--- examples/lambda.cpp
+++ examples/lambda.cpp
@@ -4,10 +4,7 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
-    vector<int> vi;
-    vi.push_back(5);
-    vi.push_back(4);
-    vi.push_back(3);
+    vector<int> vi = {5, 4 ,3};
     for_each(vi.begin(), vi.end(), [](int i) {
         cout << i << endl;
     });
--- examples/lambda_capture.cpp
+++ examples/lambda_capture.cpp
@@ -4,10 +4,7 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
-    vector<int> vi;
-    vi.push_back(1);
-    vi.push_back(2);
-    vi.push_back(3);
+    vector<int> vi = {1, 2 ,3};
 
     size_t sum = 0;
 
